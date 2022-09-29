622d8733e50395b72456fafc0b38cf4c2e0ab996
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-09-29 16:02:19 -0300

    Requisito 60

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
   return (
     <div>
       <Header title="Profile" />
@@ -13,6 +15,7 @@ function Profile() {
       <button
         type="button"
         data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
       >
         Done Recipes
       </button>
